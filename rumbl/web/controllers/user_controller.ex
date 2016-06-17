8de0348334b61783aae8db66ccc639ec2f54f049
Miguel Iñiguez Gonzale <miniguez@Miguels-MacBook-Pro.local>
2016-06-17 09:33:06 -0500

    Writing an authentication plug

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -2,10 +2,12 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
   plug :scrub_params, "user" when action in [:create, :update]
 
+
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
+    users = Repo.all(User)
     render conn, "index.html", users: users
   end
 
@@ -24,6 +26,7 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created! ")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
@@ -58,6 +61,17 @@ defmodule Rumbl.UserController do
       conn
       |> put_flash(:info, "User deleted successfully.")
       |> redirect(to: user_path(conn, :index))
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 
 end
