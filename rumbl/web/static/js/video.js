c865f2953c6ad54ea47cb5a0c4c8b86330eebe54
Miguel Iñiguez Gonzale <miniguez@Miguels-MBP.lan>
2016-07-21 11:22:49 -0500

    Creating the Channel

--- rumbl/web/static/js/video.js
+++ rumbl/web/static/js/video.js
@@ -20,6 +20,11 @@ let Video = {
     let msgInput = document.getElementById("msg-input")
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
+
+    vidChannel.join()
+    .receive("ok", resp => console.log("joined the video channel", resp) )
+    .receive("error", reason => console.log("join failed", reason) )
+
   }
 }
 export default Video
