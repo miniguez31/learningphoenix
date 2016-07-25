a53dad5222755b7936541353bd6e7b5e45a1b396
Miguel Iñiguez Gonzale <miniguez@Miguels-MacBook-Pro.local>
2016-07-25 14:58:19 -0500

    Socket Authentication

--- rumbl/web/static/js/socket.js
+++ rumbl/web/static/js/socket.js
@@ -1,11 +1,8 @@
 import {Socket} from "phoenix"
 
-let socket = new Socket(
-  "/socket",
-  {
+let socket = new Socket("/socket", {
     params: {token: window.userToken},
     logger: (kind, msg, data) => {console.log(`${kind}: ${msg}`, data)}
-  }
-)
+})
 
 export default socket
