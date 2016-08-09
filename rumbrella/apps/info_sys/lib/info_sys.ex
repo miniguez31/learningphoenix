680f375b58358cc7f9c39d9deae2446d330343ea
Miguel Iñiguez Gonzale <miniguez@Miguels-MBP.lan>
2016-08-08 23:34:27 -0500

    Testing the information system

--- rumbrella/apps/info_sys/lib/info_sys.ex
+++ rumbrella/apps/info_sys/lib/info_sys.ex
@@ -35,7 +35,7 @@ defmodule InfoSys do
   end
 
   defp await_results(children, opts) do
-    timeout = opts[:timeout] || 5000
+    timeout = opts[:timeout] || 1000
     timer = Process.send_after(self(), :timedout, timeout)
     results = await_result(children, [], :infinity)
 
