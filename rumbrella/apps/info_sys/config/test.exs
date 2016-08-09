3d8c78f38fc732a4add431af903aef2f7a59fa8a
Miguel Iñiguez Gonzale <miniguez@Miguels-MacBook-Pro.local>
2016-08-09 13:38:25 -0500

    Testing channels and OTP

---
+++ rumbrella/apps/info_sys/config/test.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :info_sys, :wolfram,
+app_id: "G722T2-8X78T59GYY",
+http_client: InfoSys.Test.HTTPClient
--- rumbrella/apps/info_sys/lib/info_sys/wolfram.ex
+++ rumbrella/apps/info_sys/lib/info_sys/wolfram.ex
@@ -22,11 +22,13 @@ defmodule InfoSys.Wolfram do
     send(owner, {:results, query_ref, results})
   end
 
+  @http Application.get_env(:info_sys, :wolfram)[:http_client] || :httpc
   defp fetch_xml(query_str) do
-   {:ok, {_, _, body}} = :httpc.request(String.to_char_list("http://api.wolframalpha.com/v2/query" <>
-   "?appid=#{app_id()}" <>
-   "&input=#{URI.encode(query_str)}&format=plaintext"))
-   body
+    {:ok, {_, _, body}} = @http.request(
+    String.to_char_list("http://api.wolframalpha.com/v2/query" <>
+    "?appid=#{app_id()}" <>
+    "&input=#{URI.encode_www_form(query_str)}&format=plaintext"))
+    body
   end
 
   defp app_id, do: Application.get_env(:info_sys, :wolfram)[:app_id]
